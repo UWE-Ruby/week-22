ae124f9acfe6446622c87e74607177f80062c5ca
Franklin Webber <franklin.webber@gmail.com>
2012-04-10 16:50:30 -0700

    FIX yml example opened the JSON file

--- lib/yml.rb
+++ lib/yml.rb
@@ -2,7 +2,7 @@ require 'YAML'
 
 configuration_root_dir = File.join(File.dirname(__FILE__),"..","fixtures")
 
-file_contents = File.read "#{configuration_root_dir}/configuration.json"
+file_contents = File.read "#{configuration_root_dir}/configuration.yml"
 
 configuration_data = YAML::load file_contents
 
